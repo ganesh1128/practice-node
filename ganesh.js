ea75cb9053e0c464307f3b2b2356b256a72065cc
gollavilli ganesh kumar <ganeshgollavilli@gmail.com>
2022-08-08 22:33:11 +0530

    new change

--- ganesh.js
+++ ganesh.js
@@ -9,3 +9,27 @@ for(i=0; i<=arr.length; i++){
     }
 }
 console.log(res);
+
+const matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+  ]
+  
+  // matrix[0].map((val, index) => matrix.map(row => row[index]).reverse())
+  
+  const n = matrix.length;
+    const x = Math.floor(n/ 2);
+    const y = n - 1;
+    for (let i = 0; i < x; i++) {
+       for (let j = i; j < y - i; j++) {
+          k = matrix[i][j];
+          matrix[i][j] = matrix[y - j][i];
+          matrix[y - j][i] = matrix[y - i][y - j];
+          matrix[y - i][y - j] = matrix[j][y - i]
+          matrix[j][y - i] = k
+          
+       }
+    }
+    console.log(matrix)
